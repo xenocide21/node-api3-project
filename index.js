ffe9517c79c7c75367cd86a879a9fb8692d554c9
Ashley Dunham <xenocide21@gmail.com>
2020-01-16 14:56:57 -0700

    updated files for deployment

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 // code away!
 const server = require('./server.js');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(5000, () => console.log(`Server listening on port ${port}`));
